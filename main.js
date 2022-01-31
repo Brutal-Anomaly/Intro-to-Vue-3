813b55bbab7d5342d454443cd1b7e5f6c1be4ab2
Brutal-Anomaly <afelder4646@yahoo.com>
2022-01-30 21:06:37 -0600

    l8 end

--- main.js
+++ main.js
@@ -4,12 +4,14 @@ const app = Vue.createApp({
             cart:0,
             product: 'Socks',
             brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
+            //image: './assets/images/socks_blue.jpg',
+            selectedVariant: 0,
+            //inStock: false,
+            onSale: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
             ]
         }
     },
@@ -17,8 +19,25 @@ const app = Vue.createApp({
         addToCart() {
             this.cart += 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) {
+            this.selectedVariant = index
+            console.log(index)
+        }
+    },
+    // this code takes data properties and turns them into computed properties
+    // the only benefit I can see to this is that it makes these variables... variable in nature
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        },
+        isOnSale() {
+            return this.brand + ' ' + this.product + ' is on sale'
         }
     }
 })
